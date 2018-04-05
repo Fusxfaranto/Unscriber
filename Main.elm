10f374e3e15f177c1559e0203a6a369f2eeb12c4
Fusxfaranto <fusxfaranto@gmail.com>
2018-04-05 15:27:51 -0400

    draw objects and capture clicks

--- Main.elm
+++ Main.elm
@@ -1,8 +1,8 @@
 module House exposing (main)
 
-import Collage exposing (Collage, styled, uniform, rotate, shiftX, shiftY)
-import Collage.Events
-import Collage.Layout exposing (spacer, stack, vertical)
+import Collage exposing (Collage, styled, uniform, rotate, shift, shiftX, shiftY)
+import Collage.Events exposing (onClick)
+import Collage.Layout exposing (align, bottomLeft, center, spacer, stack, vertical)
 import Collage.Render
 import Color exposing (..)
 import Html exposing (Html)
@@ -11,30 +11,63 @@ import Html exposing (Html)
 -- Model -----------------------------------------------------------------------
 
 
+type ObjType = Square
+             | Circle
+
+
+type alias Obj =
+    { objType: ObjType
+    , x: Int
+    , y: Int
+    , size: Int
+    , color: Color
+    }
+
+
 type alias Model =
-    { asdf: Int }
+    { textDisplay: String
+    , objs: List Obj
+    }
 
 
 init : Model
 init =
-    { asdf = 0 }
+    { textDisplay = "init"
+    , objs = [ { objType = Circle
+               , x = 3
+               , y = 7
+               , size = 2
+               , color = orange
+               }
+             , { objType = Square
+               , x = 4
+               , y = 1
+               , size = 3
+               , color = yellow
+               }
+          ]
+    }
 
 
 
 -- Update ----------------------------------------------------------------------
 
 
-type alias Msg =
-    { }
+type Msg = ObjSelect Int
 
 
 update : Msg -> Model -> Model
 update msg model =
-    { asdf = 0 }
+    case msg of
+        ObjSelect i -> {model | textDisplay = toString i}
 
 
 
 -- View ------------------------------------------------------------------------
+
+
+spacerLen : Float
+spacerLen = 50.0
 
 
 drawGrid : Int -> Collage Msg
@@ -49,19 +82,49 @@ drawGrid n =
         s = spacer 0 spacerLen
         ls = vertical <| List.intersperse l <| List.repeat n s
     in
-    stack [ls, shiftX offsetLen <| shiftY offsetLen <| rotate (degrees 90) ls]
+    align bottomLeft <| stack [ls, shiftX offsetLen <| shiftY offsetLen <| rotate (degrees 90) ls]
 
 
-genScene : Model -> Collage Msg
-genScene model = styled (uniform red, Collage.defaultLineStyle) (Collage.square 100)
+drawObj : Int -> Obj -> Collage Msg
+drawObj i o =
+    let
+        dim = (toFloat o.size) * spacerLen
+        shape = case o.objType of
+                    Square -> (Collage.square dim)
+                    Circle -> (Collage.circle <| dim / 2)
+        style = styled (uniform o.color, Collage.defaultLineStyle)
+        offsets = ((toFloat o.x) * spacerLen, (toFloat o.y) * spacerLen)
+    in
+    style shape
+        |> align bottomLeft
+        |> shift offsets
+        |> onClick (ObjSelect i)
+
+
+drawScene : Model -> Collage Msg
+drawScene model = stack <| List.indexedMap drawObj model.objs
+
+
+renderCollage : Model -> Html Msg
+renderCollage model =
+    stack
+    [ drawScene model
+    , drawGrid 10
+    ]
+        --|> Collage.scale 1
+        |> Collage.Render.svg
 
 
 view : Model -> Html Msg
 view model =
-    --genScene model
-    drawGrid 9
-        --|> Collage.scale 1
-        |> Collage.Render.svg
+    Html.table []
+        [ Html.tr []
+              [ Html.td [] [ renderCollage model ]
+              , Html.td []
+                  [ Html.p [] [Html.text <| model.textDisplay]
+                  ]
+              ]
+        ]
 
 
 
